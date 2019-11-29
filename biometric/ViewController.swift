c60049510c0388edb4b68d9cac502429d8913e91
Lammax <lammax@mail.ru>
2019-11-29 19:43:17 +0300

    Initial Commit

---
+++ biometric/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  biometric
+//
+//  Created by Mac on 29.11.2019.
+//  Copyright © 2019 Lammax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
