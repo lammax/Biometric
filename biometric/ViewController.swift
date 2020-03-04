6401791cdedccc2f4d606472d2237356ebf47315
Lammax <lammax@mail.ru>
2020-03-04 17:25:32 +0300

    Added SignIn with Apple

--- biometric/ViewController.swift
+++ biometric/ViewController.swift
@@ -6,15 +6,22 @@
 //  Copyright © 2019 Lammax. All rights reserved.
 //
 
+import Foundation
+import AuthenticationServices
 import UIKit
 import LocalAuthentication
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var authView: UIView!
+    
+    let authAppleProvider = ASAuthorizationAppleIDProvider()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        authenticationWithTouchID()
+        //authenticationWithTouchID()
+        setupAppleAuthorization()
     }
 
     func authenticationWithTouchID() {
@@ -52,6 +59,72 @@ class ViewController: UIViewController {
             print(error)
         }
     }
+    
+    func setupAppleAuthorization() {
+        setupProviderLoginView()
+    }
+    
+    func setupProviderLoginView() {
+        let button = ASAuthorizationAppleIDButton()
+        button.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+        self.authView.addSubview(button)
+    }
+    @objc func handleAuthorizationAppleIDButtonPress() {
+        let request = authAppleProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        
+        controller.performRequests()
+    }
+    
+    func setupProviderCallBack(userIdentifier: String) {
+        authAppleProvider.getCredentialState(forUserID: userIdentifier) { (credentialState, error) in
+            switch credentialState {
+            case .authorized:
+                print("AppleID is valid")
+            case .revoked:
+                print("AppleID revoked")
+            case .notFound:
+                print("AppleID not found")
+            case .transferred:
+                print("AppleID transferred")
+            }
+        }
+    }
 
+}
+
+extension ViewController: ASAuthorizationControllerDelegate {
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let credential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            let userIdentifier = credential.user
+            let identityToken = credential.identityToken
+            let authCode = credential.authorizationCode
+            let realUserStatus = credential.realUserStatus
+            let userEmail = credential.email
+            
+            print(userIdentifier, identityToken, authCode, realUserStatus, userEmail)
+
+            setupProviderCallBack(userIdentifier: userIdentifier)
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+}
+
+extension ViewController: ASAuthorizationControllerPresentationContextProviding {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
 }
 
